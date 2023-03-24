417f4b6b384ae46ea1bf6c250c2c7116a77d7665
kolia34 <mikolai46870@gmail.com>
2023-03-24 11:15:45 +0200

    змінив код

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,41 +12,32 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+        TextBox[] textBoxes;
         public Form1()
         {
             InitializeComponent();
-        }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+            textBoxes[0] = textBox1;
+            textBoxes[1] = textBox2;
+            textBoxes[2] = textBox3;
+            textBoxes[3] = textBox4;
+            textBoxes[4] = textBox5;
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-                richTextBox1.ResetText(); 
-                if (string.IsNullOrEmpty(textBox1.Text))
-                    
-                    richTextBox1.AppendText("Елемент 1: Порожньо\n");
+            richTextBox1.ResetText();
+            for (int idx = 0; idx < textBoxes.Length; idx++)
+            {
+                int num = idx + 1;
+                if (string.IsNullOrEmpty(textBoxes[idx].Text))
+                    richTextBox1.AppendText("Елемент " + num + ": Порожньо\n");
                 else
-                    
-                    richTextBox1.AppendText("Eлемент 1: " + textBox1.Text + '\n');
-               
-if (string.IsNullOrEmpty(textBox2.Text)) richTextBox1.AppendText("Елемент 2: Порожньо\n");
-            else
-                richTextBox1.AppendText("Eлемент 2: " + textBox2.Text + '\n');
-            if (string.IsNullOrEmpty(textBox3.Text)) richTextBox1.AppendText("Елемент 3: Порожньо\n");
-            else
-                richTextBox1.AppendText("EлeмеHT 3: " + textBox3.Text + '\n');
-            if (string.IsNullOrEmpty(textBox4.Text))
-                richTextBox1.AppendText("Елемент 4: Порожньо\n");
-            else
-                richTextBox1.AppendText("Eлемент 4: " + textBox4.Text + '\n');
-            if (string.IsNullOrEmpty(textBox5.Text));
-            else
-                richTextBox1.AppendText("Елемент 5: Порожньо\n");
-        
-        richTextBox1.AppendText("Enемент 5: "+textBox5.Text + '\n');
+                    richTextBox1.AppendText("Елемент " + num + "; " +
+                    textBoxes[idx].Text + '\n');
+            }
         }
     }
 }
